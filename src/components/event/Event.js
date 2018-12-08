0b7f10945eeb34e183543e4e62036f8aed3b96dc
Vu Tran <vutran6853@gmail.com>
2018-12-08 01:55:34 -0600

    clean up

--- src/components/event/Event.js
+++ src/components/event/Event.js
@@ -55,10 +55,15 @@ class Event extends Component {
          </Card>
       )
     });
-  
+    
     return (
       <div>
         { diplayEvent }
+    
+        <iframe className='iframeBox' src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=48&l=ur1&category=amzn_echo_dot_evgrn&banner=0GQJXJBFKHFB5YFH27R2&f=ifr&linkID=593b2eaf9247809f824b6423c34bef60&t=vu09-20&tracking_id=vu09-20" 
+               style={ {width:"128", height:"10", scrolling:"no" ,border:"1", marginwidth:"0" , frameborder:"0"}}>
+        </iframe>
+      
       </div>
     );
   }
